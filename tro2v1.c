65e1e60af33935c9642914e3fa66abd1c5de79f7
Terr <git@terr.nl>
2024-06-29 11:58:22 +0200

    Use constants from defines.h

--- tro2v1.c
+++ tro2v1.c
@@ -99,51 +99,11 @@ char helptext[HELPTEXT_NUM_LINES][HELPTEXT_LINE_WIDTH] = {
 static int frame_counter = 0;
 
 #define KEYBOARD_INT 0x9
-#define NUM_COLORS 256
-
-#define TEXT_PALETTE_SIZE 64
-#define TEXT_PALETTE_ANGLE 6
-#define BLACK_PALETTE_SIZE 63
-#define STARS_PALETTE_SIZE 64
-/* 2 * TEXT_PALETTE_SIZE * 3 */
-#define TEXT_PALETTE_COLORS 384
-
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-#define NUM_PIXELS 76800
-#define SCREEN_DEPTH 64
-#define PLANE_WIDTH 80
-
-#define PI 3.14159
-#define SINTABLE_SIZE 256
-#define ZTABLE_FIXED_FRAC 6
 
 #define SETPIX(x,y,c) *(VGA+(x)+(y<<8)+(y<<6))=c
 #define GETPIX(x,y) *(VGA+(x)++(y<<8)+(y<<6))
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
-
-#define NUM_LETTERS 13
-#define NUM_STARS 69
-/*#define NUM_STARS 1*/
-#define STAR_SPEED 1
-#define BITMAP_WIDTH 1080
-#define BITMAP_HEIGHT 24
-#define LETTER_SCROLL_SPEED 3
-#define LETTER_WIDTH 24
-#define LETTER_HEIGHT 24
-#define LETTER_SPACE 1056
-#define LETTER_PADDING 4
-#define LETTER_HALF_WIDTH 12
-#define LETTER_HALF_HEIGHT 12
-#define LETTER_HALF_PADDING 2
-
-
-/* (Screen height - reflection rows - margin) * pixels per plane */
-/*#define UPPER_AREA_PLANE_PIXELS (SCREEN_HEIGHT - REFLECTION_ROWS - 20) * PLANE_WIDTH*/
-#define UPPER_AREA_PLANE_PIXELS 15040
-/* (Screen height - (Screen height - reflection rows - margin)) * pixels per plane */
-#define REFLECTION_AREA_PLANE_PIXELS 4160
 
 void unrle(byte* compressed, BITMAP *bmp) {
     unsigned int i, j;
